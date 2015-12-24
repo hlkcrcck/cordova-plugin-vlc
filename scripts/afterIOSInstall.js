0a34c08512dfb4258bbddf485ed892fd951d8468
hlkcrcck <acarcicek@itu.edu.tr>
2015-12-24 15:01:35 +0200

    logg

--- scripts/afterIOSInstall.js
+++ scripts/afterIOSInstall.js
@@ -21,18 +21,28 @@ module.exports = function(context) {
   var id = context.opts.plugin.pluginInfo.id;
   if (process.env.VLC_FRAMEWORK_LOCATION===undefined) { throw new Error('environment variable VLC_FRAMEWORK_LOCATION not found'); }
   var srcFile = process.env.VLC_FRAMEWORK_LOCATION;
+  console.log("111111111");
+  console.log(srcFile);
   var frameworkFolder = srcFile.substring(srcFile.lastIndexOf('/')+1);
+  console.log("2222222222");
+  console.log(frameworkFolder);
   var pluginsPath;
   if ( /"/.test( plugins.path ) ){
     pluginsPath = plugins.path.match( /"(.*?)"/ )[1];
   } else {
     pluginsPath = pluginsPath;
   }
+  console.log("33333333");
+  console.log(pluginsPath);
   var targetDir = projectPath + pluginsPath + '/' + id + '/' + frameworkFolder;
+  console.log("444444444");
+  console.log(targetDir);
 
   if (!fs.existsSync(srcFile)) throw new Error('cannot find "' + srcFile + '" ios <framework>');
   if (fs.existsSync(targetDir)) throw new Error('target destination "' + targetDir + '" already exists');
 
+  console.log("555555555");
+  console.log(path.dirname(targetDir));
   shell.mkdir('-p', path.dirname(targetDir));
   shell.cp('-R', srcFile, path.dirname(targetDir)); // frameworks are directories
 
